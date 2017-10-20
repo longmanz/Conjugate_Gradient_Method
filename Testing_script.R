f8c05db06597cd81260e7d5871e81d44ce924d63
Longda <longmanable@aliyun.com>
2017-10-20 20:23:02 +1000

    testing script update

--- Testing_script.R
+++ Testing_script.R
@@ -10,23 +10,34 @@ source(file = "Conjugate_Gradient_Method.R")
 geno <- read.table(file = "clean_geno_pheno/clean_genotype.geno", head=T, stringsAsFactors = F)
 pheno <- read.table(file="clean_geno_pheno/simulated_phenotype.pheno", head=T, stringsAsFactors = F)
 
+idx <- which(apply(geno, 2, sd, na.rm=T) == 0 )
+geno <- geno[, -idx]
+
+geno <- as.matrix(geno)
+
+geno[!is.na(geno) & geno == 1] <- 2
 
 # Actual calculation
 
 ## !!!    One thing to be noticed is that the mouse data contians only pure strains, whose genotype is always coded as 0/1, 
 ##        where 1 is for 2 minor alleles and 0 is for 2 minor alleles. 
-##        Thus in the standardisation step, we will use 0-psqrt(2p(1-p))
+##        Thus in the standardisation step, we will use 0-p/sqrt(p(1-p))
 
 ## standardise the genotype:
-scale_geno <- function(x){
-  p <- mean(x, na.rm=T)/2
+
+for( i in 1:ncol(geno)){
+  p <- mean(geno[,i], na.rm=T)/2
+  geno[is.na(geno[,i]), i] <- p
   
+  geno[,i] <- (geno[,i]-p)/sd(geno[,i])
 }
 
-geno <- 
+
+## construct the genetic correlation matrix:
+GRM <- geno %*% t(geno)/ncol(geno)
 
 
-
+# BLUP: 
 
 
 
